cc7af77da348af609e0f5827e6b88f55dca78292
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-11-22 17:35:30 +0500

    3. posts.jsp - список вакансий.[276521#357455]

--- src/main/java/store/Store.java
+++ src/main/java/store/Store.java
@@ -2,6 +2,7 @@ package store;
 
 
 
+import model.Candidate;
 import model.Post;
 
 import java.util.Collection;
@@ -14,17 +15,26 @@ public class Store {
 
     private Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
+    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job"));
         posts.put(2, new Post(2, "Middle Java Job"));
         posts.put(3, new Post(3, "Senior Java Job"));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
     public static Store instOf() {
         return INST;
     }
 
-    public Collection<Post> findAll() {
+    public Collection<Post> findAllPosts() {
         return posts.values();
+    }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
     }
 }
