887ce6a8dd9f76af1ee586a197f800a68740cdb2
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-11-22 19:56:41 +0500

    1. Servlet. Web.xml[6866#357467]

--- src/main/java/store/Store.java
+++ src/main/java/store/Store.java
@@ -8,8 +8,11 @@ import model.Post;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Store {
+
+    private static AtomicInteger POST_ID = new AtomicInteger(4);
 
     private static final Store INST = new Store();
 
@@ -36,5 +39,11 @@ public class Store {
 
     public Collection<Candidate> findAllCandidates() {
         return candidates.values();
+    }
+
+
+    public void save(Post post) {
+        post.setId(POST_ID.incrementAndGet());
+        posts.put(post.getId(), post);
     }
 }
